85bd711651bce1ca0c9f1acf5b4c9c1c0d070438
gonzalez923 <maris_102@hotmail.com>
2025-04-02 19:34:15 -0500

    Segunda version

--- evidencia/src/App.jsx
+++ evidencia/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
   const[usuario, setUsuario] = useState('')
   const[clave, SetClave] = useState('')
+  const[logueado, setLogueado] = useState(false)
 
   function cambiarUsuario(evento){
     setUsuario(evento.target.value)
@@ -19,19 +20,28 @@ function App() {
   function ingresar(){
     if (usuario == 'admin' && clave == '123'){
     alert("inicio con exito")
+    setLogueado(true)
   }else{
     alert('usuario o clave incorrecta')
   }
 }
+if (logueado){
+  return(
+  <>
+      <h1>Registrar</h1>
+      <input type = "text" id = "usuario"/>
+      <input type = "password"  id = "clave"/>
+      <botton>Registrar</botton> 
 
-
+  </>
+  );
+}
   return (
     <>
     <h1>Inicio de Sesion</h1>
      <input type = "text" name = "usuario" id = "usuario" value ={usuario} onChange={cambiarUsuario}/>
      <input type = "password" name = "clave" id = "clave" value ={clave} onChange={cambiarClave} />
-     <botton onClick = {ingresar}>Ingresar</botton>
-
+     <botton onClick = {ingresar}>Ingresar</botton>     
     </>
   )
 }
